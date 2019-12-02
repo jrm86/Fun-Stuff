f518e1e5cac59f09cab2fcd1c820dd96152299e2
Jonathan Middleton <jrm86git@gmail.com>
2019-12-02 17:32:58 -0500

    remove typescript

---
+++ CPUSchedule/scripts/helperFunctions.js
@@ -0,0 +1,23 @@
+"use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Inclusively selects an integer number between low and high.
+ * @param low Low boundry
+ * @param high High boundry
+ */
+function randomInRange(low, high) {
+    var range = high - low;
+    if (range <= 0)
+        throw Error("High must be a number larger than Low");
+    var rand = Math.random() * 100;
+    return Math.floor(((rand) % (range + 1))) + low;
+}
+// exports.randomInRange = randomInRange;
+/**
+ * Returns a new, cloned array
+ * @param copyArr Array to be copied
+ */
+function cloneArray(copyArr) {
+    return JSON.parse(JSON.stringify(copyArr)); // this works! complete copy
+}
+// exports.cloneArray = cloneArray;
--- CPUSchedule/sketch.js
+++ CPUSchedule/sketch.js
@@ -1,30 +1,110 @@
 //Globals:
-import {ReadyQueue} from "./scripts/ReadyQueue";
-import {Schedule} from "./scripts/Schedule";
-var ReadyQueue = require("./scripts/ReadyQueue");
-var Schedule = require("./scripts/Schedule");
-
 let readyQ = new ReadyQueue();
-let sched = new Schedule();
+let sched = new Schedule(readyQ);
+let choice = "";
 
-function setup() {
-  // put setup code here
-  // sched.FCFS();
+function setup(){
+  noLoop();
+  reset();
+
+  button = createButton("Print Report");
+  button.position(30,450);
+  button.mousePressed(printReport);
+
+  button = createButton("New Set");
+  button.position(120,450);
+  button.mousePressed(resetSchedule);
+
+  rrButton = createButton("RR");
+  rrButton.position(30, 420);
+  rrButton.mousePressed(selectRR);
+
+  rrButton = createButton("FCFS");
+  rrButton.position(70, 420);
+  rrButton.mousePressed(selectFCFS);
+}
+
+function reset(){
+  createCanvas(sched.rQ.length*10+60, 500);
+  colorMode(RGB);
   background(100);
+  colorMode(HSB, 20);
 }
 
 function draw() {
   // put drawing code here
-  ellipse(60,60,60,60);
-  ellipse(50,50,50,50);
-  rect(10, 20, 30, 40);
-  // var stuff = sched.eQ;
+  clear();
+  reset();
 
-  // for(var thing in stuff){
-  //   square = stuff[thing];
+  updateChoice().then(()=>{
+    var schedule = sched.eQ;
 
+    let procs = sched.rQ.numberOfProcesses;
+    let len = sched.rQ.length;
 
-    
-  // }
+    fill(0);
+    for(let i = 0; i < procs+2; i++){
+      line(30, i*10+30, len*10+30, i*10+30);
+    }
 
+    for(var item in schedule){
+      event = schedule[item];
+      fill(event.process, 20,20);
+      rect(event.sequence*10 + 30, event.process*10+30,10,10);
+    }
+  })
+}
+
+function changeSched(newChoice){
+  return new Promise((resolve, reject)=>{
+    choice = newChoice;
+    resolve();
+  })
+}
+
+function updateChoice(){
+  return new Promise((resolve,reject)=>{
+    switch(choice){
+      case "": break;
+      case "rr": 
+        sched.RR();
+        break;
+      case "fcfs":
+        sched.FCFS();
+        break;
+    }
+    resolve();
+  })
+}
+
+function selectRR(){
+  changeSched("rr").then(()=>{
+    redraw();
+  })
+  // choice = "rr";
+  // redraw();
+}
+
+function selectFCFS(){
+  changeSched("fcfs").then(()=>{
+    redraw();
+  })
+  // choice = "fcfs";
+  // redraw();
+}
+
+function resetSchedule(){
+  readyQ = new ReadyQueue();
+  sched = new Schedule(readyQ);
+  reset();
+  redraw();
+}
+
+function printReport(){
+  let data = [];
+  data.push("Schedule: " + choice);
+  data.push("Length: " + sched.rQ.length);
+  data.push("Average time: " + sched.avgProcTime().toString());
+  data.push("Average wait: " + sched.avgWaitTime().toString());
+  save(data, 'report.txt');
 }
